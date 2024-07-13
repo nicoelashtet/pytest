d089d4fe75ccab92c53155d72566ece5fc523f7b
pannyumon <pannyum@gmail.com>
2024-07-13 10:00:49 +0900

    Add files via upload

---
+++ test_mypackage.py
@@ -0,0 +1,10 @@
+# Import pytest
+import pytest
+import NAME
+
+# Use pytest-compatible assertions
+def test_basic():
+    assert True
+
+def test_example():
+    assert True  # Use regular Python assert statement or pytest.assertion
